bd64c610598bc49684460e2de001e47a2802a4f8
Zoli1121 <32637194+Zoli1121@users.noreply.github.com>
2017-11-25 15:23:49 +0100

    Update Building.cs

--- Models/Building.cs
+++ Models/Building.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace gameproject.Models
 {
@@ -17,6 +19,8 @@ namespace gameproject.Models
 
         protected int[] buildArray;
         protected BitmapImage ImageSource;
+        protected Rectangle cell;
+        protected TextBlock toolTip;
 
         public abstract void AmortizationCounter();
         public abstract void MaintenanceCostCounter();
