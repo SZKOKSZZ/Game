eec75e4ccfbba443d9b38ec6b6b293d1b5b9fa48
Zoli1121 <32637194+Zoli1121@users.noreply.github.com>
2017-11-25 16:39:04 +0100

    Create Unit.cs
    Az összes mozgó egység őse

---
+++ Models/Unit.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace gameproject.Models
+{
+    abstract class Unit
+    {
+        protected int id;
+        protected string name;
+        protected int cost;
+        protected int Step;
+        protected int StepMax;
+        protected int Count;
+        protected int Health;  
+        protected int amortization;
+        protected int maintenanceCost;
+        protected int[] buildArray;
+        protected BitmapImage ImageSource;
+        protected Rectangle cell;
+        protected TextBlock toolTip;
+            
+    }
+}
