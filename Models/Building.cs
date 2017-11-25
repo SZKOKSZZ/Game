7af42d8758ccbaf7e1b5b1d110b309dde6a6e771
Zoli1121 <32637194+Zoli1121@users.noreply.github.com>
2017-11-25 15:07:21 +0100

    Create Building.cs
    Legyen az első osztály a Building.cs 
    Az épület (Building) absztrakt ős osztály minden épület őse. Minden speciális épület ebből származtatható. Egyelőre
    minden épületnek van egy neve, id-je, építési költsége, fenntartási költsége, amortizációja,  területe a griden,  képe (BitmapImage)

--- Models/Building.cs
+++ Models/Building.cs
@@ -8,6 +8,16 @@ namespace gameproject.Models
 {
     abstract class Building
     {
-        
+        protected int id;
+        protected string name;      
+        protected int cost;
+        protected int amortization;
+        protected int maintenanceCost;
+
+        protected int[] buildArray;
+        protected BitmapImage ImageSource;
+
+        public abstract void AmortizationCounter();
+        public abstract void MaintenanceCostCounter();
     }
 }
