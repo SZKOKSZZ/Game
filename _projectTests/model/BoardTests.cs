5d8a70c2fda83dab3a27c8b3d5b4b92995baab3e
Zoli1121 <32637194+Zoli1121@users.noreply.github.com>
2018-01-02 21:46:07 +0100

    (Unit test) _projectTests add to _project solution
    unit teszt osztály (Boardtests.cs) hozzáadása

---
+++ _projectTests/model/BoardTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _project.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _project.model.Tests
+{
+    [TestClass()]
+    public class BoardTests
+    {
+        [TestMethod()]
+        public void movePathDrawTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
